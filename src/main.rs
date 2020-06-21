7201580f6cd7c248abdb20f377ba77742c5d3e4a
Max Guppy <skylifegame@gmail.com>
2020-06-21 20:05:00 +1000

    Added file selection

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     let mut options = Options::new();
 
     options.optflag("h", "help", "Show this very helpful text");
-    options.optflag("q", "quiet", "Do not output executed commands to stdout");
+    options.optflag("q", "quiet", "Do not output executed commands to stdout"); //TODO
 
     let matches = match options.parse(env::args().skip(1)) {
         Ok(m) => m,
@@ -27,8 +27,31 @@ fn main() {
         print!("{}", options.usage("Usage: run [options] target"));
     }
 
-    let file = String::from_utf8(File::open("test/basic.run").unwrap().bytes().map(|x| x.unwrap()).collect()).unwrap();
-    match parser::RunFileParser::new().parse(Lexer::new(&mut file.chars().enumerate())) {
+    let mut runfile_path = env::current_dir().expect("Couldn't get current working directory");
+    let run_target: String;
+    if let Some(target) = matches.free.get(0) {
+        let v = target.split(':').collect::<Vec<&str>>();
+        
+        if v.len() == 1 {
+            runfile_path.push("run");
+            run_target = v[0].to_owned();
+        } else if v.len() == 2 {
+            let mut s = v[0].to_owned();
+            s.push_str(".run");
+            runfile_path.push(s);
+            run_target = v[1].to_owned();
+        } else {
+            panic!("Invalid target");
+        }
+    } else {
+        runfile_path.push("run");
+        run_target = String::new();
+    }
+
+    let mut file = String::new();
+    File::open(runfile_path).expect("Failed to open file").read_to_string(&mut file).expect("Failed to read file");
+
+    match parser::RunFileParser::new().parse(Lexer::new(&mut file.char_indices())) {
         Ok(rf) => {
             for command in rf.global_target.unwrap().commands {
                 println!("{:?}", command); //TODO: Implement Display
@@ -38,7 +61,7 @@ fn main() {
                     } else {
                         unimplemented!();
                     }
-                })).status().expect("Failed to execute command");
+                })).status().expect("Failed to execute command"); //TODO
             }
         },
         Err(e) => {
