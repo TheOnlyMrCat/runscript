8a8f7ac18a52aeb0115113f1db8bf52adcd7f1b5
TheOnlyMrCat <signupforforums5@gmail.com>
2020-06-22 13:56:54 +1000

    Implemented Display for Command

--- src/runfile.rs
+++ src/runfile.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::fmt::{self, Display, Formatter};
+use std::mem::discriminant;
 
 #[derive(Debug)]
 pub struct RunFile {
@@ -18,11 +20,49 @@ pub struct Command {
     pub args: Vec<Argument>,
 }
 
-pub type Argument = Vec<ArgPart>;
+#[derive(Debug)]
+pub struct Argument {
+    pub parts: Vec<ArgPart>
+}
 
 #[derive(Debug)]
 pub enum ArgPart {
     Str(String),
     Var(String),
     Cmd(Command),
+}
+
+impl ArgPart {
+    fn arg_str(&self) -> String {
+        if let ArgPart::Str(s) = self {
+            s.clone()
+        } else {
+            panic!("Expected Str argument");
+        }
+    }
+}
+
+impl Display for Command {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}{}", self.target, self.args.iter().fold("".to_owned(), |mut acc, x| {
+            acc.push(' ');
+            acc + &format!("{}", x)
+        }))
+    }
+}
+
+impl Display for Argument {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        if self.parts.iter().any(|x| discriminant(x) != discriminant(&ArgPart::Str("".to_owned()))) {
+            write!(f, "\"{}\"", self.parts.iter().fold("".to_owned(), |acc, part| match part {
+                ArgPart::Str(s) => acc + s,
+                ArgPart::Var(v) => { acc + &format!("${}", v) },
+                ArgPart::Cmd(c) => { acc + &format!("$({})", c) },
+            }))
+        } else if self.parts.iter().any(|x| x.arg_str().contains(' ')) {
+            write!(f, "'{}'", self.parts.iter().fold("".to_owned(), |acc, part| acc + &part.arg_str()))
+        } else {
+            write!(f, "{}", self.parts.iter().fold("".to_owned(), |acc, part| acc + &part.arg_str()))
+        }
+    }
 }
