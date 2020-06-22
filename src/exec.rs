035bbdf369d7e843f9807e68dfc50af1b5d431df
Max Guppy <skylifegame@gmail.com>
2020-06-22 13:56:54 +1000

    Implemented Display for Command

--- src/exec.rs
+++ src/exec.rs
@@ -6,13 +6,13 @@ use crate::Config;
 pub fn shell(commands: &Vec<runfile::Command>, config: &Config) {
     for command in commands {
         if !config.silent {
-            println!("\t{:?}", command); //TODO: Implement Display
+            println!("\t{}", command);
         }
         let status = Command::new(command.target.clone())
             .args(
                 command.args.iter()
                     .map(|x| {
-                        if let runfile::ArgPart::Str(s) = &x[0] {
+                        if let runfile::ArgPart::Str(s) = &x.parts[0] {
                             s
                         } else {
                             unimplemented!();
@@ -28,8 +28,8 @@ pub fn shell(commands: &Vec<runfile::Command>, config: &Config) {
             .expect("Failed to execute command"); //TODO
         if !config.silent {
             match status.code() {
-                Some(i) => println!("\t`{:?}` exited with code {}", command, i),
-                None => println!("\t`{:?}` terminated by signal", command), //TODO figure out which signal
+                Some(i) => println!("\t`{}` exited with code {}", command, i),
+                None => println!("\t`{}` terminated by signal", command), //TODO figure out which signal
             }
         }
         if !status.success() {
