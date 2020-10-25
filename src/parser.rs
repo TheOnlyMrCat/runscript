09aee01067d7b945300c3dcb6163a4e8c7b382a0
TheOnlyMrCat <signupforforums5@gmail.com>
2020-10-26 08:31:59 +1100

    Restructure script API

--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,16 @@
+use std::collections::HashMap;
 use std::ops::Range;
 use std::path::Path;
 
-use crate::runfile::RunFileRef;
+use crate::script::{Runscript, Scripts};
 
-pub struct RunscriptSource<'a> {
+pub struct RunscriptSource {
 	/// The name of the runfile this runscript belongs to, for location-tracking purposes.
 	name: String,
 	/// The include-nesting index of this runscript
 	index: Vec<usize>,
 	/// The runscript code to be parsed. Generally the contents of a runfile.
-	source: &'a str,
+	source: String,
 }
 
 pub struct RunscriptLocation {
@@ -24,32 +25,47 @@ pub enum RunscriptParseError {
 	IOError(std::io::Error),
 }
 
-pub fn parse_runfile(path: impl AsRef<Path>) -> Result<RunFileRef, RunscriptParseError> {
+pub fn parse_runfile(path: impl AsRef<Path>) -> Result<Runscript, RunscriptParseError> {
 	parse_runscript(RunscriptSource {
 		name: path.as_ref().file_name()
 			.ok_or(RunscriptParseError::InvalidPath)?
 			.to_string_lossy()
 			.into_owned(),
 		index: Vec::new(),
-		source: &std::fs::read_to_string(path).map_err(|e| RunscriptParseError::IOError(e))?,
+		source: std::fs::read_to_string(path).map_err(|e| RunscriptParseError::IOError(e))?,
 	})
 }
 
-pub fn parse_runfile_nested(path: impl AsRef<Path>, index: Vec<usize>) -> Result<RunFileRef, RunscriptParseError> {
+pub fn parse_runfile_nested(path: impl AsRef<Path>, index: Vec<usize>) -> Result<Runscript, RunscriptParseError> {
 	parse_runscript(RunscriptSource {
 		name: path.as_ref().file_name()
 			.ok_or(RunscriptParseError::InvalidPath)?
 			.to_string_lossy()
 			.into_owned(),
 		index: Vec::new(),
-		source: &std::fs::read_to_string(path).map_err(|e| RunscriptParseError::IOError(e))?,
+		source: std::fs::read_to_string(path).map_err(|e| RunscriptParseError::IOError(e))?,
 	})
 }
 
-struct ParsingContext {
-	
+struct ParsingContext<T: Iterator<Item = (usize, char)>> {
+	iterator: std::iter::Peekable<T>,
+	runfile: Runscript,
 }
 
-pub fn parse_runscript(source: RunscriptSource) -> Result<RunFileRef, RunscriptParseError> {
+pub fn parse_runscript(source: RunscriptSource) -> Result<Runscript, RunscriptParseError> {
+	let mut context = ParsingContext {
+		iterator: source.source.char_indices().peekable(),
+		runfile: Runscript {
+			name: source.name,
+			line_ends: source.source.char_indices().filter_map(|(index, ch)| if ch == '\n' { Some(index) } else { None }).collect(),
+			source: source.source,
+			includes: Vec::new(),
+			scripts: Scripts {
+				global_target: HashMap::new(),
+				default_target: HashMap::new(),
+				targets: HashMap::new(),
+			}
+		}
+	};
 	todo!();
 }
